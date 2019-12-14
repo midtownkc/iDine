35f5835545f98ecca8ac980640d07ee3389212dd
Maxwell Frederickson <maxwell@apple.com>
2019-12-14 12:40:46 -0600

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Maxwell Frederickson on 12/14/19.
+//  Copyright © 2019 Hacking With Swift. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
